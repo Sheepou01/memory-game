4a4376f4642a7c4b08dd7dc2f827341a931c10c2
Sheepou01 <sebastienantic9@gmail.com>
2018-12-07 22:38:44 +0100

    étape 4 ok

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ var app = {
         // je crée mon tableau de cartes
         var setOfCards = [];
 
+        //je crée un tableau de paire pour mes comparaisons
+        app.cardsToCompare = []; 
+
         // je crée une boucle qui va générer mes cartes
         for (var nb = 0; nb < 28; nb++) {
             var card = $('<div class="carte cache"></div>');
@@ -26,14 +29,26 @@ var app = {
         // J'ajoute mon jeu de cartes au plateau
         $('#plateau').append(setOfCards);
 
+        
         $('.carte').on('click', app.showCard);
+        
+        
+        
     },
 
     showCard: function(evt) {
         var card = evt.currentTarget;
         
         $(card).removeClass('cache').addClass('image');
+        app.cardsToCompare.push(card);
+        
 
+        if (app.cardsToCompare.length == 2) {
+            $('.carte').off('click');
+            app.isPaire(app.cardsToCompare);
+            
+        }
+        console.log(app.cardsToCompare.length)
     },
 
     shuffle: function(array) {
@@ -45,7 +60,33 @@ var app = {
         array[j] = x;
     }
     return array;
+    },
+
+    //Fonction qui va gérer s'il s'agit de paire ou non
+    isPaire: function(array) {
+        
+        if (array[0].outerHTML == array[1].outerHTML) {
+            alert('Bravo');
+            app.cardsToCompare = [];
+            $('.carte').on('click', app.showCard);
+            
+        } else {
+            window.setTimeout(app.hideCards, 1000);
+            
+        }
+    }, 
+
+    //Fonction qui se charge de recacher les cartes
+    hideCards: function() {
+        console.log('salut');
+        $(app.cardsToCompare).each(function() {
+            $(this).removeClass('image').addClass('cache');
+        });
+        app.cardsToCompare = [];
+        $('.carte').on('click', app.showCard);
     }
+
+
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
